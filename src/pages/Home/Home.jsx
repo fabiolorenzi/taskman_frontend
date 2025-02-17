444139b0fe7ffb3b80475bb69bf6e32323e8d7ee
fabiolorenzi <fabiolorenzi13975@gmail.com>
2025-02-17 09:22:24 +0000

    created basic homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import { Helmet } from "react-helmet";
 
 import "./Home.scss";
 
+import Logo from "../../media/logo.png"
+
 function Home() {
 
     useEffect(() => {
@@ -16,6 +18,24 @@ function Home() {
             </Helmet>
             <div className="home_container">
                 <div className="home_body">
+                    <div className="home_top">
+                        <div className="home_title">
+                            <h1>Taskman</h1>
+                        </div>
+                        <div className="home_underTitle">
+                            <h2>Manage easily your projects</h2>
+                        </div>
+                    </div>
+                    <div className="home_bottom">
+                        <div className="home_bottomBody">
+                            <div className="home_bottomText">
+                                <p>Using Taskman, you can organize every task for your team. Work faster, work better, work smarter!</p>
+                            </div>
+                            <div className="home_bottomImage">
+                                <img src={Logo} alt="userImage" />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
