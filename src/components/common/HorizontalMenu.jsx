fae221d1fdd328e78142430bb364b07352b0da90
fabiolorenzi <fabiolorenzi13975@gmail.com>
2025-02-26 11:34:04 +0000

    deactivate settings link in horizontal header component

--- src/components/common/HorizontalMenu.jsx
+++ src/components/common/HorizontalMenu.jsx
@@ -8,7 +8,7 @@ function HorizontalMenu() {
             <div className="horizontalMenu_container">
                 <NavLink to="/dashboard" title="Dashboard">Dashboard</NavLink>
                 <NavLink to="/projects" title="Projects">Projects</NavLink>
-                <NavLink to="/settings" title="Settings">Settings</NavLink>
+                {/*<NavLink to="/settings" title="Settings">Settings</NavLink>*/}
                 <NavLink to="/logout" title="Logout">Logout</NavLink>
             </div>
         </div>
